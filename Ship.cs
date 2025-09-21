652bc6835759cf48111f52e4dc8964f46f673efb
Laura Keeley <laura@Lauras-MacBook-Pro.local>
2025-09-21 16:34:33 -0500

    git init

---
+++ Ship.cs
@@ -0,0 +1,39 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Spaceship;
+
+public class Ship
+{
+    static public Vector2 defaultPosition = new Vector2(640, 360);
+    public Vector2 position = defaultPosition;
+    public int speed = 180;
+    public int radius = 30;
+
+    public void shipUpdate(GameTime gameTime)
+    {
+        KeyboardState kState = Keyboard.GetState();
+        float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        
+        if (kState.IsKeyDown(Keys.Right) && position.X < 1280)
+        {
+            position.X += speed * dt;
+        }
+        
+        if (kState.IsKeyDown(Keys.Left) && position.X > 0)
+        {
+            position.X -= speed * dt;
+        }
+        
+        if (kState.IsKeyDown(Keys.Up) && position.Y > 0)
+        {
+            position.Y -= speed * dt;
+        }
+        
+        if (kState.IsKeyDown(Keys.Down) && position.Y < 720)
+        {
+            position.Y += speed * dt;
+        }
+    }
+}
